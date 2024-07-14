694da998bf2daef17286fe7c37ae7b36fd6f5087
TriPTM <tridien121@gmail.com>
2024-07-14 18:01:06 +0700

    BagsDao && ProductDao

--- AppleShopProject/src/java/ModelDao/BagsDao.java
+++ AppleShopProject/src/java/ModelDao/BagsDao.java
@@ -1,37 +1,45 @@
-
 package ModelDao;
 
 import Connection.DBConnection;
 import Model.Bags;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 public class BagsDao {
-    public static ArrayList<Bags> getAllBags() {
+
+    public static ArrayList<Bags> getAllBags() throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
         ArrayList<Bags> list = new ArrayList<Bags>();
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Bags_getAll()}");
-            ResultSet rs = cstmt.executeQuery();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("Select * from Orders");
             while (rs.next()) {
                 list.add(new Bags(rs.getInt("UserID"), rs.getInt("ProductID"), rs.getBoolean("Status"),
-                rs.getInt("Stock")));
+                        rs.getInt("Stock")));
             }
-            cstmt.close();
             con.close();
         } catch (Exception e) {
             System.out.println("Error" + e);
+        } finally {
+            stmt.close();
+            rs.close();
         }
         return list;
     }
 
-    public static int addBags(Bags bags){
+    public static int addBags(Bags bags) {
         int status = 0;
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Bags_addBags(?,?,?,?)}");
+            PreparedStatement cstmt = con.prepareCall("insert into Bags(UserID,ProductID,Status,Stock)"
+                    + "values(?,?,?,?)");
             cstmt.setInt(1, bags.getUserID());
             cstmt.setInt(2, bags.getProductID());
             cstmt.setBoolean(3, bags.isStatus());
@@ -45,12 +53,13 @@ public class BagsDao {
         }
         return status;
     }
-    
-    public static int updateBags(Bags bags){
+
+    public static int updateBags(Bags bags) {
         int status = 0;
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Bags_updateBags(?,?,?,?)}");
+            PreparedStatement cstmt = con.prepareCall("update Bags set Status = ?, Stock = ?"
+                    + "where UserID = ? and ProductID = ?");
             cstmt.setInt(1, bags.getUserID());
             cstmt.setInt(2, bags.getProductID());
             cstmt.setBoolean(3, bags.isStatus());
@@ -64,12 +73,12 @@ public class BagsDao {
         }
         return status;
     }
-    
-    public static int deleteBags(int ProductID, int UserID){
+
+    public static int deleteBags(int ProductID, int UserID) {
         int status = 0;
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Bags_deleteBags(?,?)}");
+            PreparedStatement cstmt = con.prepareCall("delete from Bags where UserID = ? and ProductID = ?");
             cstmt.setInt(1, UserID);
             cstmt.setInt(2, ProductID);
             ResultSet rs = cstmt.executeQuery();
@@ -81,18 +90,18 @@ public class BagsDao {
         }
         return status;
     }
-    
-    public static Bags getBagsbyID(int ProductID, int UserID){
+
+    public static Bags getBagsbyID(int ProductID, int UserID) {
         Bags bags = new Bags();
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Bags_getByBagsID(?,?)}");
+            PreparedStatement cstmt = con.prepareCall("select * from Bags where UserID = ? and ProductID = ?");
             cstmt.setInt(1, UserID);
             cstmt.setInt(2, ProductID);
             ResultSet rs = cstmt.executeQuery();
             while (rs.next()) {
                 bags = new Bags(rs.getInt("UserID"), rs.getInt("ProductID"), rs.getBoolean("Status"),
-                rs.getInt("Stock"));
+                        rs.getInt("Stock"));
             }
             cstmt.close();
             con.close();
--- AppleShopProject/src/java/ModelDao/ProductDao.java
+++ AppleShopProject/src/java/ModelDao/ProductDao.java
@@ -3,27 +3,31 @@ package ModelDao;
 
 import Connection.DBConnection;
 import Model.Product;
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.*;
 
 public class ProductDao {
-    public static ArrayList<Product> getAllProduct() {
+    public static ArrayList<Product> getAllProduct() throws SQLException {
+        Statement stmt = null;
+        ResultSet rs = null;
         ArrayList<Product> list = new ArrayList<Product>();
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Product_getAll()}");
-            ResultSet rs = cstmt.executeQuery();
+            stmt = con.createStatement();
+            rs = stmt.executeQuery("Select * from Product");
             while (rs.next()) {
                 list.add(new Product(rs.getInt("ProductID"), rs.getString("ProductName"), rs.getInt("CategoryID"),
                 rs.getFloat("Price"), rs.getInt("Stock"), rs.getString("DateUpdate"), rs.getBoolean("Status"),
                 rs.getString("ImageURL"), rs.getString("Description"), rs.getString("MetaContent")));
             }
-            cstmt.close();
             con.close();
         } catch (Exception e) {
             System.out.println("Error" + e);
+        }finally {
+            stmt.close();
+            rs.close();
         }
         return list;
     }
@@ -32,7 +36,8 @@ public class ProductDao {
         int status = 0;
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Product_addProduct(?,?,?,?,?,?,?,?)}");
+            PreparedStatement cstmt = con.prepareCall("Insert into Product(ProductName,CategoryID,Price,Stock,Status,ImageURL,Description,MetaContent)"
+                    + " values (?,?,?,?,?,?,?,?)");
             cstmt.setString(1, product.getProductName());
             cstmt.setInt(2, product.getCategoryID());
             cstmt.setFloat(3, product.getMoney());
@@ -55,12 +60,15 @@ public class ProductDao {
         int status = 0;
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Product_updateProduct(?,?,?,?,?,?,?,?,?)}");
-            cstmt.setInt(1, product.getProductID());
-            cstmt.setString(2, product.getProductName());
-            cstmt.setInt(3, product.getCategoryID());
-            cstmt.setFloat(4, product.getMoney());
-            cstmt.setInt(5, product.getStock());
+            PreparedStatement cstmt = con.prepareCall("update Product set ProductName = ?, CategoryID = ?, "
+                    + "Price = ?, Stock = ?, DateUpdate = ?, Status = ?, ImageURL = ?, "
+                    + "Description = ?, MetaContent = ? where ProductID = ?");
+            cstmt.setInt(10, product.getProductID());
+            cstmt.setString(1, product.getProductName());
+            cstmt.setInt(2, product.getCategoryID());
+            cstmt.setFloat(3, product.getMoney());
+            cstmt.setInt(4, product.getStock());
+            cstmt.setString(5, product.getDateUpdate());
             cstmt.setBoolean(6, product.isStatus());
             cstmt.setString(7, product.getImageURL());
             cstmt.setString(8, product.getDescription());
@@ -79,7 +87,7 @@ public class ProductDao {
         int status = 0;
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Product_deleteProduct(?)}");
+            PreparedStatement cstmt = con.prepareCall("Delete from Product where ProductID=?");
             cstmt.setInt(1, ProductID);
             ResultSet rs = cstmt.executeQuery();
             status = cstmt.executeUpdate();
@@ -95,7 +103,7 @@ public class ProductDao {
         Product product = new Product();
         try {
             Connection con = DBConnection.getConnection();
-            CallableStatement cstmt = con.prepareCall("{call usp_Product_getByProductID(?)}");
+            PreparedStatement cstmt = con.prepareCall("Select* from Product where ProductID=?");
             cstmt.setInt(1, ProductID);
             ResultSet rs = cstmt.executeQuery();
             while (rs.next()) {
